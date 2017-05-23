d0f74f35bdbabea76519cbfa357838b72dadac6c
jason.wong <jason.wong@jasonwongs-MacBook-Pro.local>
2017-05-23 18:03:39 +0800

    2017-05-23b

--- src/va_client/component/component/photopicker/photo.picker.set.load.or.del.dlg.js
+++ src/va_client/component/component/photopicker/photo.picker.set.load.or.del.dlg.js
@@ -29,23 +29,59 @@ var PhotoPickerSetLoadOrDeleteDlgComponent = PhotoPickerSetLoadOrDeleteDlgCompon
         _this._element = _element;
         _this._renderer = _renderer;
         _this._coreModel = _coreModel;
-        _this._frm = {};
         _this._isLoadMode = true;
-        /* TODO: get from a Mock/Normal service instead of hard-code */
-        _this._frm['selectedSet'] = [
-            { 'id': '001', "name": "bikini set 01", "checked": false, "photo.list": ['http://modelisto.com/photo/tara-booher-model-123835.jpg', 'http://static.face.nextmedia.com/images/next-photos/face/235/640pixfolder/W1035_307__DSC6389.jpg'] },
-            { 'id': '002', "name": "doom game 01", "checked": true, "photo.list": ['https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg'] },
-            { 'id': '003', "name": "quake game 99", "checked": true, "photo.list": ['http://3.bp.blogspot.com/-n7HdvzSs-Hg/U2YpvpQ_E9I/AAAAAAAAIcc/q1rfRlsbQJ0/s1600/4.jpg', 'http://2.bp.blogspot.com/-vBpZvMiQJnU/U2YpuFX-JzI/AAAAAAAAIcU/vFurHF7IB-s/s1600/2.jpg'] }
-        ];
+        // did the preview pane show up
+        _this._isPreviewMode = false;
+        // css related objects
+        _this._divSetItemList = { 'text-truncate': false,
+            'col-sm-12': true, 'col-md-12': true,
+            'photo-picker-set-selection-item-list-pane-left': false
+        };
+        _this._divSetPreview = { 'hiding': true,
+            'photo-picker-set-selection-item-list-pane-right': false
+        };
+        _this._setPreviewPhotoData = '../../../static/img/photoPicker/emptyImage.gif';
+        _this._frm = { 'setChoices': [] };
         return _this;
     }
     PhotoPickerSetLoadOrDeleteDlgComponent.prototype.ngAfterContentChecked = function () {
-        console.log(this._frm);
+        this._getAvailableSet();
+    };
+    PhotoPickerSetLoadOrDeleteDlgComponent.prototype.ngOnDestroy = function () {
+        this._reset();
+    };
+    PhotoPickerSetLoadOrDeleteDlgComponent.prototype._getAvailableSet = function () {
+        if (this._frm['selectedSet'] == null || this._frm['selectedSet'].length == 0) {
+            /* TODO: get from a Mock/Normal service instead of hard-code
+              sorted by latest creation date (most up to date ones at the top)
+            */
+            this._frm['selectedSet'] = [
+                { 'id': '001', "name": "bikini set 01", "checked": false, "photo.list": ['http://modelisto.com/photo/tara-booher-model-123835.jpg', 'http://static.face.nextmedia.com/images/next-photos/face/235/640pixfolder/W1035_307__DSC6389.jpg'] },
+                { 'id': '002', "name": "doom game 01", "checked": false, "photo.list": ['https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg'] },
+                { 'id': '003', "name": "quake game 99", "checked": false, "photo.list": ['http://3.bp.blogspot.com/-n7HdvzSs-Hg/U2YpvpQ_E9I/AAAAAAAAIcc/q1rfRlsbQJ0/s1600/4.jpg', 'http://2.bp.blogspot.com/-vBpZvMiQJnU/U2YpuFX-JzI/AAAAAAAAIcU/vFurHF7IB-s/s1600/2.jpg'] }
+            ];
+        }
+        // set back the "checked" property when necessary
+        if (this._frm['setChoices']) {
+            // key is "id"; only store "id" thas is selected
+            for (var _i = 0, _a = this._frm['selectedSet']; _i < _a.length; _i++) {
+                var _item = _a[_i];
+                var _val = this._frm['setChoices'][_item['id']];
+                if (_val && _val == true) {
+                    _item['checked'] = true;
+                }
+                else {
+                    _item['checked'] = false;
+                }
+            } // end -- for
+        } // end -- if (_frm[setChoices] valid)
+        return this._frm['selectedSet'];
     };
     PhotoPickerSetLoadOrDeleteDlgComponent.prototype._reset = function () {
         this._isLoadMode = true;
         this._frm = {
-            'selectedSet': []
+            'selectedSet': [],
+            'setChoices': []
         };
     };
     /* ------------------ */
@@ -55,9 +91,57 @@ var PhotoPickerSetLoadOrDeleteDlgComponent = PhotoPickerSetLoadOrDeleteDlgCompon
         console.log('btn one clicked');
     };
     PhotoPickerSetLoadOrDeleteDlgComponent.prototype.buttonTwoClick = function (_e) {
-        console.log('btn two clicked');
-        this._reset();
+        // check if require to reset or not (done by ngOnDestroy)
         PhotoPickerSetLoadOrDeleteDlgComponent_1.hideDlg(this.getDlg(this._element, 'dlgPhotoPickerSetLoad'), this._renderer);
+    };
+    /* ------------------ */
+    /*  event handlers    */
+    /* ------------------ */
+    PhotoPickerSetLoadOrDeleteDlgComponent.prototype.onSetItemClick = function (_e, _id, _index) {
+        if (_index > -1 && this._frm['selectedSet'] &&
+            _index < this._frm['selectedSet'].length) {
+            // toggle
+            var _checked = !this._frm['selectedSet'][_index]['checked'];
+            if (_checked == true) {
+                this._frm['setChoices'][_id] = true;
+            }
+            else {
+                delete this._frm['setChoices'][_id];
+            }
+            /*
+             *  since _frm['setChoices'] is not observable (non ng-model binded)
+             *  hence, you need to update the "checked" property to get a view refresh
+             */
+            this._frm['selectedSet'][_index]['checked'] = _checked;
+        } // end -- if (_index is valid)
+    };
+    PhotoPickerSetLoadOrDeleteDlgComponent.prototype.onPreviewSetClick = function (_e, _id, _index) {
+        _e.preventDefault();
+        _e.stopPropagation();
+        console.log('** preview button clicked > ' + _index);
+        if (this._isPreviewMode == false) {
+            this._divSetItemList = {
+                'text-truncate': true, 'col-sm-6': true, 'col-md-6': true,
+                'photo-picker-set-selection-item-list-pane-left': true
+            };
+            this._divSetPreview = {
+                'col-sm-6': true, 'col-md-6': true, 'showing': true,
+                'photo-picker-set-selection-item-list-pane-right': true
+            };
+            this._isPreviewMode = true;
+        }
+    };
+    PhotoPickerSetLoadOrDeleteDlgComponent.prototype.hidePreviewPane = function () {
+        this._isPreviewMode = false;
+        this._divSetItemList = {
+            'text-truncate': false, 'col-sm-6': false, 'col-md-6': false,
+            'col-sm-12': true, 'col-md-12': true,
+            'photo-picker-set-selection-item-list-pane-left': false
+        };
+        this._divSetPreview = {
+            'col-sm-6': false, 'col-md-6': false, 'hiding': true, 'showing': false,
+            'photo-picker-set-selection-item-list-pane-right': false
+        };
     };
     return PhotoPickerSetLoadOrDeleteDlgComponent;
 }(generic_dlg_component_1.GenericDlgComponent));
--- src/va_client/component/component/photopicker/photo.picker.set.load.or.del.dlg.ts
+++ src/va_client/component/component/photopicker/photo.picker.set.load.or.del.dlg.ts
@@ -1,6 +1,6 @@
 import { Component, ElementRef, Renderer2, Input } from '@angular/core';
 
-import { NgIf, NgFor } from '@angular/common';
+import { NgIf, NgFor, NgClass } from '@angular/common';
 
 import { CoreModel } from './../../core/coreModel';
 import { CoreModelProvider } from './../../core/core.model.provider';
@@ -25,8 +25,21 @@ export class PhotoPickerSetLoadOrDeleteDlgComponent extends GenericDlgComponent 
 
   // parent component (could be anything, hence didn't state the "type")
   @Input() _parent:any;
-  private _frm:any={};
+  private _frm:any;
   private _isLoadMode:boolean=true;
+  // did the preview pane show up
+  private _isPreviewMode:boolean=false;
+
+  // css related objects
+  private _divSetItemList:any = { 'text-truncate': false,
+    'col-sm-12': true, 'col-md-12': true,
+    'photo-picker-set-selection-item-list-pane-left': false
+  };
+  private _divSetPreview:any = { 'hiding':true,
+    'photo-picker-set-selection-item-list-pane-right': false
+  };
+
+  private _setPreviewPhotoData:string='../../../static/img/photoPicker/emptyImage.gif';
 
 
   /**
@@ -37,21 +50,47 @@ export class PhotoPickerSetLoadOrDeleteDlgComponent extends GenericDlgComponent 
     private _coreModel:CoreModel) {
     super();
 
-/* TODO: get from a Mock/Normal service instead of hard-code */
-    this._frm['selectedSet']=[
-      { 'id': '001', "name": "bikini set 01", "checked": false, "photo.list": ['http://modelisto.com/photo/tara-booher-model-123835.jpg', 'http://static.face.nextmedia.com/images/next-photos/face/235/640pixfolder/W1035_307__DSC6389.jpg'] },
-      { 'id': '002', "name": "doom game 01", "checked": true, "photo.list": ['https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg'] },
-      { 'id': '003', "name": "quake game 99", "checked": true, "photo.list": ['http://3.bp.blogspot.com/-n7HdvzSs-Hg/U2YpvpQ_E9I/AAAAAAAAIcc/q1rfRlsbQJ0/s1600/4.jpg', 'http://2.bp.blogspot.com/-vBpZvMiQJnU/U2YpuFX-JzI/AAAAAAAAIcU/vFurHF7IB-s/s1600/2.jpg'] }
-    ];
+    this._frm={ 'setChoices': [] };
   }
   ngAfterContentChecked() {
-    console.log(this._frm);
+    this._getAvailableSet();
+  }
+  ngOnDestroy() {
+    this._reset();
+  }
+
+  private _getAvailableSet() {
+    if (this._frm['selectedSet']==null || this._frm['selectedSet'].length==0) {
+/* TODO: get from a Mock/Normal service instead of hard-code
+  sorted by latest creation date (most up to date ones at the top)
+*/
+      this._frm['selectedSet']=[
+        { 'id': '001', "name": "bikini set 01", "checked": false, "photo.list": ['http://modelisto.com/photo/tara-booher-model-123835.jpg', 'http://static.face.nextmedia.com/images/next-photos/face/235/640pixfolder/W1035_307__DSC6389.jpg'] },
+        { 'id': '002', "name": "doom game 01", "checked": false, "photo.list": ['https://upload.wikimedia.org/wikipedia/en/5/57/Doom_cover_art.jpg'] },
+        { 'id': '003', "name": "quake game 99", "checked": false, "photo.list": ['http://3.bp.blogspot.com/-n7HdvzSs-Hg/U2YpvpQ_E9I/AAAAAAAAIcc/q1rfRlsbQJ0/s1600/4.jpg', 'http://2.bp.blogspot.com/-vBpZvMiQJnU/U2YpuFX-JzI/AAAAAAAAIcU/vFurHF7IB-s/s1600/2.jpg'] }
+      ];
+    }
+    // set back the "checked" property when necessary
+    if (this._frm['setChoices']) {
+      // key is "id"; only store "id" thas is selected
+      for (let _item of this._frm['selectedSet']) {
+        let _val:boolean = this._frm['setChoices'][_item['id']];
+        if (_val && _val==true) {
+          _item['checked']=true;
+        } else {
+          _item['checked']=false;
+        }
+      } // end -- for
+    } // end -- if (_frm[setChoices] valid)
+
+    return this._frm['selectedSet'];
   }
 
   private _reset() {
     this._isLoadMode=true;
     this._frm={
-      'selectedSet': []
+      'selectedSet': [],
+      'setChoices': []
     };
   }
 
@@ -62,12 +101,61 @@ export class PhotoPickerSetLoadOrDeleteDlgComponent extends GenericDlgComponent 
     console.log('btn one clicked');
   }
   protected buttonTwoClick(_e:Event):void {
-    console.log('btn two clicked');
-
-    this._reset();
+    // check if require to reset or not (done by ngOnDestroy)
     PhotoPickerSetLoadOrDeleteDlgComponent.hideDlg(
       this.getDlg(this._element, 'dlgPhotoPickerSetLoad'),
       this._renderer);
+  }
+
+  /* ------------------ */
+  /*  event handlers    */
+  /* ------------------ */
+
+  private onSetItemClick(_e:Event, _id:string, _index:number) {
+    if (_index > -1 && this._frm['selectedSet'] &&
+      _index < this._frm['selectedSet'].length) {
+      // toggle
+      let _checked:boolean=!this._frm['selectedSet'][_index]['checked'];
+      if (_checked==true) {
+        this._frm['setChoices'][_id]=true;
+      } else {
+        delete this._frm['setChoices'][_id];
+      }
+      /*
+       *  since _frm['setChoices'] is not observable (non ng-model binded)
+       *  hence, you need to update the "checked" property to get a view refresh
+       */
+      this._frm['selectedSet'][_index]['checked']=_checked;
+    } // end -- if (_index is valid)
+  }
+  private onPreviewSetClick(_e:Event, _id:string, _index:number) {
+    _e.preventDefault();
+    _e.stopPropagation();
+    console.log('** preview button clicked > '+_index);
+
+    if (this._isPreviewMode==false) {
+      this._divSetItemList={
+        'text-truncate': true, 'col-sm-6': true, 'col-md-6': true,
+        'photo-picker-set-selection-item-list-pane-left': true
+      };
+      this._divSetPreview={
+        'col-sm-6': true, 'col-md-6': true, 'showing': true,
+        'photo-picker-set-selection-item-list-pane-right': true
+      };
+      this._isPreviewMode=true;
+    }
+  }
+  private hidePreviewPane() {
+    this._isPreviewMode=false;
+    this._divSetItemList={
+      'text-truncate': false, 'col-sm-6': false, 'col-md-6': false,
+      'col-sm-12': true, 'col-md-12': true,
+      'photo-picker-set-selection-item-list-pane-left': false
+    };
+    this._divSetPreview={
+      'col-sm-6': false, 'col-md-6': false, 'hiding': true, 'showing': false,
+      'photo-picker-set-selection-item-list-pane-right': false
+    };
   }
 
 
